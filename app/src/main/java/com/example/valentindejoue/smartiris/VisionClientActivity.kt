25bb82eeb69a968edd35c4a16c9903971d24bc2d
valentin.dejoue <valentin.dejoue@spie.com>
2018-09-18 16:43:07 +0200

    Init Smart Iris

---
+++ app/src/main/java/com/example/valentindejoue/smartiris/VisionClientActivity.kt
@@ -0,0 +1,13 @@
+package com.example.valentindejoue.smartiris
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v7.widget.CardView
+
+class VisionClientActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_visioclient_select)
+    }
+}
